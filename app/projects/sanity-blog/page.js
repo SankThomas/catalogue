a9727e3d1ee796c33aa5902799e12516db4c03e3
sankthomas <thomassankara74@gmail.com>
2024-07-19 00:33:04 +0300

    blogposts homepage

---
+++ app/projects/sanity-blog/page.js
@@ -0,0 +1,31 @@
+import Container from "@/components/container";
+import { CardDescription } from "@/components/ui/card";
+import React from "react";
+import Posts from "./_components/posts";
+
+export default function SanityBlog() {
+  return (
+    <>
+      <Container>
+        <div className="mb-8 text-center">
+          <h1>Blog using Sanity CMS</h1>
+          <CardDescription>
+            This project is built using{" "}
+            <a
+              href="https://sanity.io"
+              target="_blank"
+              rel="noreferrer"
+              className="text-white underline"
+            >
+              Sanity CMS
+            </a>
+          </CardDescription>
+        </div>
+
+        <div>
+          <Posts />
+        </div>
+      </Container>
+    </>
+  );
+}
