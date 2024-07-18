a88b66d427e77ccadf9ffb52948a83866eeef20c
sankthomas <thomassankara74@gmail.com>
2024-07-19 00:28:33 +0300

    update className

--- app/projects/countries/_components/searchcountries.js
+++ app/projects/countries/_components/searchcountries.js
@@ -12,7 +12,7 @@ export default function SearchCountries({ searchCountries }) {
           id="search"
           placeholder="Search for a country"
           autoComplete="off"
-          className="w-full rounded-lg border border-neutral-900 bg-transparent px-6 py-3 text-neutral-400 placeholder-neutral-600 outline-none transition"
+          className="input"
           onChange={(e) => searchCountries(e.target.value)}
         />
       </div>
