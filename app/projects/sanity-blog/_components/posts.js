532c8295fec8851acbfff6879744aade1a546379
sankthomas <thomassankara74@gmail.com>
2024-07-20 16:25:43 +0300

    change to client component and add search functionality

--- app/projects/sanity-blog/_components/posts.js
+++ app/projects/sanity-blog/_components/posts.js
@@ -1,6 +1,8 @@
+"use client";
+
 import { createClient, groq } from "next-sanity";
 import { client } from "@/client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { format } from "date-fns";
@@ -11,66 +13,107 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Search from "@/components/search";
 
-async function getPosts() {
-  return createClient(client).fetch(
-    groq`*[_type == "post"] {
-      title,
-      slug,
-      body,
-      publishedAt,
-      categories[] -> {
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [text, setText] = useState("");
+
+  // posts.map((post) => console.log(post.categories));
+
+  function search(title) {
+    setText(title);
+
+    if (text) {
+      const filteredPosts = posts.filter((post) =>
+        Object.values(post)
+          .join("")
+          .toLowerCase()
+          .includes(title.toLowerCase()),
+      );
+      setFilteredPosts(filteredPosts);
+    } else {
+      setFilteredPosts(posts);
+    }
+  }
+
+  async function getPosts() {
+    const allPosts = await createClient(client).fetch(
+      groq`*[_type == "post"] {
         title,
-      },
-      mainImage {
-        asset -> {
-          url
+        slug,
+        body,
+        publishedAt,
+        categories[] -> {
+          title,
         },
-        alt,
-      },
-      "name": author -> name,
-    } | order(publishedAt desc)`,
-  );
-}
+        mainImage {
+          asset -> {
+            url
+          },
+          alt,
+        },
+        "name": author -> name,
+      } | order(publishedAt desc)`,
+    );
+    setPosts(allPosts);
+  }
 
-export default async function Posts() {
-  const posts = await getPosts();
-
-  posts.map((post) => console.log(post.categories));
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <>
-      <h2 className="mb-8 text-3xl font-bold">{posts.length} blog posts</h2>
+      <div className="mb-12 space-y-6 text-center">
+        <CardDescription>
+          This is my actual blog on{" "}
+          <a
+            href="https://tsbsankara.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white underline"
+          >
+            my website
+          </a>
+        </CardDescription>
+
+        <div className="mx-auto max-w-2xl">
+          <Search search={search} />
+        </div>
+      </div>
 
       <div className="grid gap-4 lg:grid-cols-2 lg:gap-8">
-        {posts.map((post) => (
-          <Link
-            key={post.slug.current}
-            href={`/projects/sanity-blog/posts/${post.slug.current}`}
-          >
-            <Card>
-              <CardHeader>
-                {post.mainImage ? (
-                  <Image
-                    src={post.mainImage.asset.url}
-                    width={800}
-                    height={600}
-                    alt={post.mainImage.alt}
-                    className="aspect-auto h-auto w-full object-contain object-center lg:h-96"
-                  />
-                ) : (
-                  <CardTitle>No cover image</CardTitle>
-                )}
-              </CardHeader>
-              <CardContent className="space-y-4">
-                <CardTitle>{post.title}</CardTitle>
-                <CardDescription>
-                  By {post.name} &middot;{" "}
-                  {format(new Date(post.publishedAt), "do MMMM yyyy")}
-                </CardDescription>
-                <CardDescription>{`${post.body[0].children[0].text.substring(0, 200)}...`}</CardDescription>
+        {text.length > 1
+          ? filteredPosts.map((post) => (
+              <Link
+                key={post.slug.current}
+                href={`/projects/sanity-blog/posts/${post.slug.current}`}
+              >
+                <Card>
+                  <CardHeader>
+                    {post.mainImage ? (
+                      <Image
+                        src={post.mainImage.asset.url}
+                        width={800}
+                        height={600}
+                        alt={post.mainImage.alt}
+                        className="aspect-auto h-auto w-full object-contain object-center lg:h-96"
+                      />
+                    ) : (
+                      <CardTitle>No cover image</CardTitle>
+                    )}
+                  </CardHeader>
+                  <CardContent className="space-y-4">
+                    <CardTitle>{post.title}</CardTitle>
+                    <CardDescription>
+                      By {post.name} &middot;{" "}
+                      {format(new Date(post.publishedAt), "do MMMM yyyy")}
+                    </CardDescription>
+                    <CardDescription>{`${post.body[0].children[0].text.substring(0, 200)}...`}</CardDescription>
 
-                {/* {post.categories !== null && (
+                    {/* {post.categories !== null && (
                 <ul className="flex items-center justify-start gap-2">
                   {post.categories.map((category, index) => (
                     <li
@@ -82,10 +125,53 @@ export default async function Posts() {
                   ))}
                 </ul>
               )} */}
-              </CardContent>
-            </Card>
-          </Link>
-        ))}
+                  </CardContent>
+                </Card>
+              </Link>
+            ))
+          : posts.map((post) => (
+              <Link
+                key={post.slug.current}
+                href={`/projects/sanity-blog/posts/${post.slug.current}`}
+              >
+                <Card>
+                  <CardHeader>
+                    {post.mainImage ? (
+                      <Image
+                        src={post.mainImage.asset.url}
+                        width={800}
+                        height={600}
+                        alt={post.mainImage.alt}
+                        className="aspect-auto h-auto w-full object-contain object-center lg:h-96"
+                      />
+                    ) : (
+                      <CardTitle>No cover image</CardTitle>
+                    )}
+                  </CardHeader>
+                  <CardContent className="space-y-4">
+                    <CardTitle>{post.title}</CardTitle>
+                    <CardDescription>
+                      By {post.name} &middot;{" "}
+                      {format(new Date(post.publishedAt), "do MMMM yyyy")}
+                    </CardDescription>
+                    <CardDescription>{`${post.body[0].children[0].text.substring(0, 200)}...`}</CardDescription>
+
+                    {/* {post.categories !== null && (
+                <ul className="flex items-center justify-start gap-2">
+                  {post.categories.map((category, index) => (
+                    <li
+                      key={index}
+                      className="inline rounded-full bg-neutral-900 px-2 py-[2px] text-xs text-neutral-400"
+                    >
+                      {category.title}
+                    </li>
+                  ))}
+                </ul>
+              )} */}
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
       </div>
     </>
   );
