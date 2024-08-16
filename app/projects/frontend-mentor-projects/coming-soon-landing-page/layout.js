3963477b890671467f9c67c165be81e923c5fd2f
Thomas Sankara <thomassankara74@gmail.com>
2024-08-16 21:32:26 +0300

    coming soon landing page

---
+++ app/projects/frontend-mentor-projects/coming-soon-landing-page/layout.js
@@ -0,0 +1,10 @@
+import { Josefin_Sans } from "next/font/google";
+
+const josefinsans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
+export default function ComingSoonLandingPageLayout({ children }) {
+  return <div className={josefinsans.className}>{children}</div>;
+}
