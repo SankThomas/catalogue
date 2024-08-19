8c94866e7be64b03105421395ca49c81e4b8de32
SankThomas <thomassankara74@gmail.com>
2024-08-19 14:25:39 +0300

    pokemon details page

--- app/projects/pokedex/[pokemon]/page.js
+++ app/projects/pokedex/[pokemon]/page.js
@@ -1,46 +1,58 @@
+"use client";
+
 import Container from "@/components/container";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "../_components/chart";
 
 export default async function page({ params }) {
+  const [details, setDetails] = useState();
+
+  console.log(params);
+
   async function getPokemonDetails() {
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${params.pokemon}`,
     );
-
-    return res.json();
+    const data = await res.json();
+    setDetails(data);
   }
 
-  const details = await getPokemonDetails();
+  useEffect(() => {
+    getPokemonDetails();
+  }, [params]);
 
   return (
     <Container>
-      <div className="grid gap-8 lg:grid-cols-2 lg:items-center">
-        <article>
-          <h1>{details.name}</h1>
+      {details ? (
+        <div className="mt-8 grid gap-8 lg:grid-cols-2 lg:items-center">
+          <article>
+            <h1>{details.name}</h1>
 
-          <Image
-            src={details.sprites.other.home.front_default}
-            width={400}
-            height={400}
-            alt={details.name}
-          />
+            <Image
+              src={details.sprites.other.home.front_default}
+              width={400}
+              height={400}
+              alt={details.name}
+            />
 
-          <ul className="mt-8 flex flex-wrap items-center justify-start gap-4">
-            {details.types.map((type, index) => (
-              <li key={index}>
-                <Button>{type.type.name}</Button>
-              </li>
-            ))}
-          </ul>
-        </article>
+            <ul className="mt-8 flex flex-wrap items-center justify-start gap-4">
+              {details.types.map((type, index) => (
+                <li key={index}>
+                  <Button>{type.type.name}</Button>
+                </li>
+              ))}
+            </ul>
+          </article>
 
-        <article>
-          <Chart />
-        </article>
-      </div>
+          <article>
+            <Chart details={details} />
+          </article>
+        </div>
+      ) : (
+        <p className="mt-8">Loading pokemon details</p>
+      )}
     </Container>
   );
 }
