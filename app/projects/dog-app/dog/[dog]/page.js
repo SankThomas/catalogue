4ae8143e6fbd0bb688f75cd62d1e96cc48e3921b
SankThomas <thomassankara74@gmail.com>
2024-08-19 16:30:31 +0300

    check whether dog exists and then render it to the screen

--- app/projects/dog-app/dog/[dog]/page.js
+++ app/projects/dog-app/dog/[dog]/page.js
@@ -23,62 +23,66 @@ export default async function Dog({ params }) {
   return (
     <>
       <Container>
-        <div className="mt-12 grid gap-4 md:grid-cols-2 lg:gap-8">
-          <Image
-            src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
-            alt={dog.name}
-            width={800}
-            height={600}
-            className="w-full rounded-lg object-cover"
-          />
+        {dog ? (
+          <div className="mt-12 grid gap-4 md:grid-cols-2 lg:gap-8">
+            <Image
+              src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
+              alt={dog.name}
+              width={800}
+              height={600}
+              className="w-full rounded-lg object-cover"
+            />
 
-          <div className="space-y-4">
-            <h1>{dog.name}</h1>
             <div className="space-y-4">
-              <CardDescription>
-                <strong>Origin:</strong> {dog.origin}
-              </CardDescription>
-              <CardDescription>
-                <strong>Bred For:</strong> {dog.bred_for}
-              </CardDescription>
-              <CardDescription>
-                <strong>Breed Group:</strong> {dog.breed_group}
-              </CardDescription>
-              <CardDescription>
-                <strong>Life Span:</strong> {dog.life_span}
-              </CardDescription>
-              <CardDescription>
-                <strong>Temperament:</strong> {dog.temperament}
-              </CardDescription>
-            </div>
+              <h1>{dog.name}</h1>
+              <div className="space-y-4">
+                <CardDescription>
+                  <strong>Origin:</strong> {dog.origin}
+                </CardDescription>
+                <CardDescription>
+                  <strong>Bred For:</strong> {dog.bred_for}
+                </CardDescription>
+                <CardDescription>
+                  <strong>Breed Group:</strong> {dog.breed_group}
+                </CardDescription>
+                <CardDescription>
+                  <strong>Life Span:</strong> {dog.life_span}
+                </CardDescription>
+                <CardDescription>
+                  <strong>Temperament:</strong> {dog.temperament}
+                </CardDescription>
+              </div>
 
-            <div className="grid grid-cols-2 gap-4">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Height</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <CardDescription>
-                    {dog.height.imperial} inches
-                  </CardDescription>
-                  <CardDescription>{dog.height.metric} cm</CardDescription>
-                </CardContent>
-              </Card>
+              <div className="grid grid-cols-2 gap-4">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Height</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>
+                      {dog.height.imperial} inches
+                    </CardDescription>
+                    <CardDescription>{dog.height.metric} cm</CardDescription>
+                  </CardContent>
+                </Card>
 
-              <Card>
-                <CardHeader>
-                  <CardTitle>Weight</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <CardDescription>
-                    {dog.weight.imperial} pounds
-                  </CardDescription>
-                  <CardDescription>{dog.weight.metric} kgs</CardDescription>
-                </CardContent>
-              </Card>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Weight</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>
+                      {dog.weight.imperial} pounds
+                    </CardDescription>
+                    <CardDescription>{dog.weight.metric} kgs</CardDescription>
+                  </CardContent>
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <p>Loading dog details</p>
+        )}
       </Container>
     </>
   );
