96a61dafd2126f4edb8df10a67edd2b48f75f079
SankThomas <thomassankara74@gmail.com>
2024-08-19 14:26:06 +0300

    chartjs component

--- app/projects/pokedex/_components/chart.js
+++ app/projects/pokedex/_components/chart.js
@@ -21,20 +21,27 @@ ChartJs.register(
   Legend,
 );
 
-export default function Chart() {
+export default function Chart({ details }) {
   const data = {
     labels: [
-      "HP",
-      "Attack",
-      "Defense",
-      "Special Attack",
-      "Special Defense",
-      "Speed",
+      `${details.stats[0].stat.name.toUpperCase()}`,
+      `${details.stats[1].stat.name.toUpperCase()}`,
+      `${details.stats[2].stat.name.toUpperCase()}`,
+      `${details.stats[3].stat.name.toUpperCase()}`,
+      `${details.stats[4].stat.name.toUpperCase()}`,
+      `${details.stats[5].stat.name.toUpperCase()}`,
     ],
     datasets: [
       {
-        label: "Stats for ${pokemon name}",
-        data: [45, 49, 49, 65, 65, 45],
+        label: `${details.name.toUpperCase()}`,
+        data: [
+          `${details.stats[0].base_stat}`,
+          `${details.stats[1].base_stat}`,
+          `${details.stats[2].base_stat}`,
+          `${details.stats[3].base_stat}`,
+          `${details.stats[4].base_stat}`,
+          `${details.stats[5].base_stat}`,
+        ],
         backgroundColor: [
           "rgba(75, 192, 192, 0.2)",
           "rgba(255, 99, 132, 0.2)",
