dd0e61136e5a2d5883c39167a0184a64fef72c8e
SankThomas <thomassankara74@gmail.com>
2024-08-19 14:26:28 +0300

    add search filter, increase pokemon limit

--- app/projects/pokedex/page.js
+++ app/projects/pokedex/page.js
@@ -1,57 +1,84 @@
 "use client";
 
 import Container from "@/components/container";
+import Search from "@/components/search";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import PokemonCard from "./_components/pokemoncard";
 
 export default function page() {
   const [pokemon, setPokemon] = useState();
-  const [total] = useState(5);
+  const [filteredCards, setFilteredCards] = useState([]);
+  const [text, setText] = useState("");
+
+  async function getPokemon() {
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
+      const data = await res.json();
+      setPokemon(data.results);
+    } catch {
+      console.error("There was a problem fetching data");
+    }
+  }
 
   useEffect(() => {
-    // Function to loop through the total number provided
-    async function fetchPokemon() {
-      for (let i = 1; i <= total; i++) {
-        await getPokemon(i);
-      }
-    }
-
-    async function getPokemon(id) {
-      try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-        const data = await res.json();
-        setPokemon(data);
-      } catch {
-        throw new Error("Failed to fetch from the PokeAPI");
-      }
-    }
-
-    fetchPokemon();
+    getPokemon();
   }, []);
+
+  function search(title) {
+    setText(title);
+
+    if (text) {
+      const filteredCards = pokemon.filter((poke) =>
+        Object.values(poke)
+          .join("")
+          .toLocaleLowerCase()
+          .includes(title.toLowerCase()),
+      );
+      setFilteredCards(filteredCards);
+    } else {
+      setFilteredCards(pokemon);
+    }
+  }
 
   return (
     <Container>
       <article className="mx-auto mb-8 max-w-lg space-y-4 text-center">
         <h1>Pokedex</h1>
-        <p>Click on a card to view more information about the Pokemon</p>
+        <p>
+          Click on a card to view more information about the Pokemon. This app
+          only shows the first 1000 pokemon in the Pokedex.
+        </p>
+
+        <Search search={search} />
       </article>
 
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
-        {/* {pokemon.map((result, index) => (
-          <Link key={result.name} href={`/projects/pokedex/${index + 1}`}>
-            <Card>
-              <CardHeader>
-                <CardTitle className="capitalize">{result.name}</CardTitle>
-              </CardHeader>
-            </Card>
-          </Link>
-        ))} */}
-
-        <PokemonCard pokemon={pokemon} />
+        {pokemon ? (
+          text.length > 1 ? (
+            filteredCards.map((result) => (
+              <Link key={result.name} href={`/projects/pokedex/${result.name}`}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle className="capitalize">{result.name}</CardTitle>
+                  </CardHeader>
+                </Card>
+              </Link>
+            ))
+          ) : (
+            pokemon.map((result) => (
+              <Link key={result.name} href={`/projects/pokedex/${result.name}`}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle className="capitalize">{result.name}</CardTitle>
+                  </CardHeader>
+                </Card>
+              </Link>
+            ))
+          )
+        ) : (
+          <p>Loading pokemon data</p>
+        )}
       </div>
     </Container>
   );
