afe6b4a6d35481e21f8fa53176f716c438497f06
SankThomas <thomassankara74@gmail.com>
2024-09-12 01:31:30 +0300

    resize images

--- app/projects/sports-app/teams/[teams]/page.js
+++ app/projects/sports-app/teams/[teams]/page.js
@@ -52,7 +52,7 @@ export default async function Teams({ params }) {
                       width={400}
                       height={400}
                       alt={team.strTeam}
-                      className="mx-auto block h-32 w-32 md:h-[400px] md:w-[400px]"
+                      className="mx-auto block"
                     />
                   ) : (
                     <CardDescription>No team badge</CardDescription>
