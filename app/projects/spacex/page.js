eac7ec42b9b005b34763369e47432579cbc3a441
SankThomas <thomassankara74@gmail.com>
2024-09-12 02:03:43 +0300

    homepage

--- app/projects/spacex/page.js
+++ app/projects/spacex/page.js
@@ -1,5 +1,63 @@
+import { PersonIcon } from "@radix-ui/react-icons";
+import { CalendarIcon, LocateIcon, RocketIcon } from "lucide-react";
 import React from "react";
 
-export default function SpaceX() {
-  return <div>SpaceX</div>;
+async function getDetails() {
+  const res = await fetch("https://api.spacexdata.com/v4/company");
+
+  if (!res.ok) throw new Error("Failed to fetch");
+
+  return res.json();
+}
+
+export default async function SpaceX() {
+  const details = await getDetails();
+
+  return (
+    <div>
+      {details ? (
+        <div className="mx-auto max-w-2xl space-y-6 text-center">
+          <h1 className="text-4xl font-bold lg:text-6xl">SpaceX</h1>
+          <p className="text-muted-foreground">{details.summary}</p>
+
+          <h2 className="text-2xl font-bold lg:text-3xl">Company details</h2>
+          <ul className="flex flex-wrap items-center justify-center gap-4">
+            <li className="flex flex-col items-center gap-4 rounded-lg border border-neutral-800 p-4 font-normal text-muted-foreground">
+              <span className="block font-semibold">
+                <LocateIcon className="text-white" />
+              </span>
+              Located at {details.headquarters.address},{" "}
+              {details.headquarters.city}, {details.headquarters.state}
+            </li>
+            <li className="flex flex-col items-center gap-4 rounded-lg border border-neutral-800 p-4 font-normal text-muted-foreground">
+              <span className="block font-semibold">
+                <CalendarIcon className="text-white" />
+              </span>
+              and founded in {details.founded}
+            </li>
+            <li className="flex flex-col items-center gap-4 rounded-lg border border-neutral-800 p-4 font-normal text-muted-foreground">
+              <span className="block font-semibold">
+                <PersonIcon className="text-white" />
+              </span>
+              By {details.founder}
+            </li>
+            <li className="flex flex-col items-center gap-4 rounded-lg border border-neutral-800 p-4 font-normal text-muted-foreground">
+              <span className="block font-semibold">
+                <PersonIcon className="text-white" />
+              </span>
+              It has {details.employees} employees
+            </li>
+            <li className="flex flex-col items-center gap-4 rounded-lg border border-neutral-800 p-4 font-normal text-muted-foreground">
+              <span className="block font-semibold">
+                <RocketIcon className="text-white" />
+              </span>
+              and {details.vehicles} rockets
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <p>Loading details...</p>
+      )}
+    </div>
+  );
 }
