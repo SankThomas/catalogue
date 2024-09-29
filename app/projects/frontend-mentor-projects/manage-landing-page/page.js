a1b715c39ee7b406aa92807dda8013f4e421223c
SankThomas <thomassankara74@gmail.com>
2024-09-29 15:24:52 +0300

    add back button component

--- app/projects/frontend-mentor-projects/manage-landing-page/page.js
+++ app/projects/frontend-mentor-projects/manage-landing-page/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./_components/header";
 import Showcase from "./_components/showcase";
+import Container from "@/components/fmcontainer";
 
 export default function ManageLandingPage() {
   return (
     <section className="h-screen bg-white">
+      <Container />
       <div className="container mx-auto">
         <Header />
         <Showcase />
